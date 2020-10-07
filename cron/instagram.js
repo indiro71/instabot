2af2be9e83268fb8bf5a5bd41e6e1ada170bb89d
Mickle <mickle@indiro.ru>
2020-10-07 11:27:26 +0300

    class

--- cron/instagram.js
+++ cron/instagram.js
@@ -1,7 +1,7 @@
 const CronJob = require('cron').CronJob;
 
 const Instagram = require('../models/Instagram');
-const inst = require('../helpers/instagram');
+const instagram = require('../helpers/Instagram');
 
 const instagramSubUnsub = new CronJob('10 */12 * * *', async function () {
     try {
@@ -9,13 +9,7 @@ const instagramSubUnsub = new CronJob('10 */12 * * *', async function () {
 
         for (const { name, password, countSubscribe, countUnSubscribe, active } of accounts) {
         	if (name && password && active && (countSubscribe || countUnSubscribe)) {
-                await inst.initialize();
-                await inst.login(name, password);
 
-                if (countSubscribe) await inst.subscribe(countSubscribe);
-                if (countUnSubscribe) await inst.unsubscribe(name, countUnSubscribe);
-
-                await inst.close();
             }
         }
     } catch (e) {
@@ -27,12 +21,10 @@ const instagramLikes = new CronJob('*/30 * * * *', async function () {
     try {
         const accounts = await Instagram.find();
 
-        for (const { name, password, countLikes, active, tagLikes = [] } of accounts) {
-        	if (name && password && tagLikes.length > 0 && active) {
-                await inst.initialize(false);
-                await inst.login(name, password);
-                await inst.liked(tagLikes, countLikes);
-                await inst.close();
+        for (const account of accounts) {
+        	if (account.name && account.password && account.tagLikes.length > 0 && account.active) {
+                const inst = new instagram(account);
+                await inst.clickLikes();
             }
         }
     } catch (e) {
@@ -45,12 +37,10 @@ const test = async () => {
 	try {
         const accounts = await Instagram.find();
 
-        for (const { name, password, countLikes, tagLikes = [], tested } of accounts) {
-        	if (name && password && tagLikes.length > 0 && tested) {
-                await inst.initialize(false);
-                await inst.login(name, password);
-                await inst.liked(tagLikes, 1);
-                await inst.close();
+        for (const account of accounts) {
+        	if (account.name && account.password && account.tagLikes.length > 0 && account.tested) {
+                const inst = new instagram(account);
+                await inst.clickLikes();
             }
         }
     } catch (e) {
@@ -60,5 +50,5 @@ const test = async () => {
 
 // test();
 
-instagramSubUnsub.start();
+// instagramSubUnsub.start();
 instagramLikes.start();
---
+++ helpers/Instagram.js
@@ -0,0 +1,198 @@
+const puppeteer = require('puppeteer');
+const http = require('http');
+const fs = require('fs');
+
+class Instagram {
+    constructor(account, mobile = true, useProxy = false) {
+        this.name = account.name;
+        this.password = account.password;
+        this.countSubscribe = account.countSubscribe || 5;
+        this.countUnSubscribe = account.countUnSubscribe || 5;
+        this.countLikes = account.countLikes || 5;
+        this.tagLikes = account.tagLikes || [];
+        this.active = account.active || true;
+        this.tested = account.tested || false;
+        this.private = account.private || true;
+        this.test = account.test || false;
+
+        this.mobile = mobile;
+        this.useProxy = useProxy;
+        this.headless = this.test;
+
+        this.browser = 'null';
+        this.page = null;
+        this.base_url = 'https://www.instagram.com';
+        this.user_agent = 'Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile Safari/537.36';
+    }
+
+    async initialize() {
+        const options = {};
+        options.headless = this.headless;
+
+        if (this.mobile) {
+            options.defaultViewport = {
+                width: 320,
+                height: 570
+            }
+        }
+
+        options.args = [
+            '--no-sandbox',
+            '--lang=en-EN,en'
+        ];
+
+        if (this.useProxy) {
+            options.args.push('--proxy-server=IP:PORT');
+        }
+
+        this.browser = await puppeteer.launch(options);
+        this.page = await this.browser.newPage();
+
+        if (this.useProxy) {
+            await this.page.authenticate({
+                username: 'username',
+                password: 'password',
+            });
+        }
+
+        await this.page.setExtraHTTPHeaders({
+            'Accept-Language': 'en'
+        });
+
+        if (this.mobile) {
+            await this.page.setUserAgent(this.user_agent);
+        }
+    }
+
+    async login() {
+        if (this.name && this.password) {
+            await this.page.goto(`${this.base_url}/accounts/login/?source=auth_switcher`, { waitUntil: 'networkidle2' });
+            await this.page.waitForSelector('input[name="username"]');
+
+            await this.page.type('input[name="username"]', this.name, { delay: 10 });
+            await this.page.type('input[name="password"]', this.password, { delay: 10 });
+            await this.page.click('button[type="submit"]');
+
+            await this.page.waitForXPath('//button[contains(text(),\'Save Info\')]');
+            await this.page.screenshot({ path: 'temp/afterLoginPage.png' });
+        }
+    }
+
+    async subscribe() {
+        if (!this.countSubscribe) return false;
+
+        await this.page.goto(`${this.base_url}/explore/people/suggested/`);
+        await this.page.waitForXPath('//button[contains(text(),\'Follow\')]');
+
+        const buttons = await this.page.$x('//button[contains(text(),\'Follow\')]');
+
+        for (let i = 0; i < this.countSubscribe; i++) {
+            let button = buttons[i];
+            await button.click();
+        }
+    }
+
+    async unsubscribe() {
+        if (!this.countUnSubscribe) return false;
+
+        await this.page.goto(`${this.base_url}/${this.name}/`);
+        await this.page.waitForSelector('img[alt="Change Profile Photo"]');
+
+        const subsButton = await this.page.$x('//a[text()[contains(.,"following")]]');
+        await subsButton[0].click();
+
+        await this.page.waitForXPath("//button[contains(text(),'Following')]");
+        const buttons = await this.page.$x("//button[contains(text(),'Following')]");
+
+        for (let i = 0; i < this.countUnSubscribe; i++) {
+            let button = buttons[i];
+
+            await button.click();
+            await this.page.waitForXPath("//button[contains(text(),'Unfollow')]");
+            const unsubButton = await this.page.$x("//button[contains(text(),'Unfollow')]");
+            await unsubButton[0].click();
+        }
+    }
+
+    async postdata(imgUrl, caption = '') {
+        if (!imgUrl) return false;
+
+        await this.liked();
+
+        const filePath = 'temp/newpost.jpg';
+        const file = fs.createWriteStream(filePath);
+        const request = http.get(imgUrl.replace('https', 'http'), function(response) {
+            response.pipe(file);
+        });
+
+        await this.page.goto(`${this.base_url}/${this.name}/`);
+        await this.page.waitForSelector('img[alt="Change Profile Photo"]');
+
+        await this.page.waitForSelector("input[type='file']");
+        let fileInputs = await this.page.$$('input[type="file"]');
+        let input = fileInputs[fileInputs.length-1];
+
+        await this.page.click("[aria-label='New Post']");
+        await this.page.waitFor(3000);
+
+        await input.uploadFile(filePath);
+        await this.page.waitFor(10000);
+
+        await this.page.waitForXPath("//button[contains(text(),'Next')]");
+        let next = await this.page.$x("//button[contains(text(),'Next')]");
+        await next[0].click();
+
+        if (caption) {
+            await this.page.waitForSelector("textarea[aria-label='Write a caption…']");
+            await this.page.click("textarea[aria-label='Write a caption…']");
+            await this.page.keyboard.type(caption);
+        }
+
+        await this.page.waitForXPath("//button[contains(text(),'Share')]");
+        let share = await this.page.$x("//button[contains(text(),'Share')]");
+        await share[0].click();
+
+        await this.page.waitFor(10000);
+    }
+
+    async liked() {
+        if (this.tagLikes.length === 0) return false;
+
+        for (let tag of this.tagLikes) {
+            await this.page.goto(`${this.base_url}/explore/tags/${tag}/`);
+            await this.page.waitForSelector("article > div img");
+
+            // const images = await instagram.page.$$('article div img[decoding="auto"]'); // top posts
+            const images = await this.page.$$('article>div:nth-child(3) img[decoding="auto"]');    //most recent posts
+
+            for (let i = 0; i < this.countLikes; i++) {
+                let image = images[i];
+
+                await image.click();
+
+                await this.page.waitForSelector('button[aria-hidden="true"]');
+                await this.page.waitForSelector('span svg[aria-label="Comment"]');
+
+                if (await this.page.$('span svg[aria-label="Like"]')) {
+                    await this.page.click('span svg[aria-label="Like"]');
+                }
+
+                await this.page.click('button svg[aria-label="Close"]');
+            }
+        }
+    }
+
+    async close() {
+        await this.browser.close();
+    }
+
+    async clickLikes() {
+        this.mobile = false;
+        await this.initialize();
+        await this.login();
+        await this.liked();
+        await this.close();
+    }
+}
+
+module.exports = Instagram;
